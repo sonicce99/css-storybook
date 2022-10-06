4ef2791d979691e286a69a372b433462c8f77df5
dongsu <sonicce99@naver.com>
2022-10-06 16:32:59 +0900

    docs : Custom ScrollBar 생성

---
+++ stories/Scrollbar.stories.jsx
@@ -0,0 +1,10 @@
+import ScrollBar from "../components/ScrollBar";
+
+export default {
+  title: "dongsu/ScrollBar",
+  component: ScrollBar,
+};
+
+const Template = (args) => <ScrollBar {...args} />;
+
+export const Default = Template.bind({});
