374c41d382ebe5d5c687d81108a2177b51d01476
dongsu <sonicce99@naver.com>
2022-12-01 12:37:20 +0900

    feat: 태극기 stories 개발 완료

--- stories/SouthKorea.stories.tsx
+++ stories/SouthKorea.stories.tsx
@@ -4,8 +4,23 @@ import { ComponentMeta, ComponentStory } from "@storybook/react";
 export default {
   title: "dongsu/South-Korea",
   component: App,
+
+  parameters: {
+    docs: {
+      description: { component: "2022 월드컵을 기념하여 만든 태극기 입니다." },
+    },
+  },
+
+  argTypes: {
+    width: {
+      control: { type: "number", min: 300, max: 800, step: 20 },
+    },
+  },
 } as ComponentMeta<typeof App>;
 
-const Template: ComponentStory<typeof App> = () => <App />;
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  width: 800,
+};
