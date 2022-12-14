8dff9e4426926482bc319e97b84feffe11bdea53
dongsu <sonicce99@naver.com>
2022-12-14 16:34:34 +0900

    feat: Tooltip 배치 완료

--- components/Tooltip/index.tsx
+++ components/Tooltip/index.tsx
@@ -4,22 +4,69 @@ import Tooltip from "./Tooltip";
 
 interface Props {
   text: string;
-  buttonText: string;
   backgroundColor: string;
-  placement: string;
 }
 
-const App: React.FC<Props> = ({
-  text,
-  buttonText,
-  backgroundColor,
-  placement,
-}) => {
+const App: React.FC<Props> = ({ text }) => {
   return (
     <Container>
-      <Tooltip text={text} placement={placement} color={backgroundColor}>
-        <Button>{buttonText}</Button>
-      </Tooltip>
+      <Flex>
+        <Tooltip text={text} placement="topLeft" color="pink">
+          <Button>TL</Button>
+        </Tooltip>
+
+        <Tooltip text={text} placement="top" color="red">
+          <Button>Top</Button>
+        </Tooltip>
+
+        <Tooltip text={text} placement="topRight" color="yellowgreen">
+          <Button>TR</Button>
+        </Tooltip>
+      </Flex>
+
+      <Between>
+        <FlexReverse>
+          <Tooltip text={text} placement="leftTop" color="orange">
+            <Button>LT</Button>
+          </Tooltip>
+
+          <Tooltip text={text} placement="left" color="#28bdb3">
+            <Button>Left</Button>
+          </Tooltip>
+
+          <Tooltip text={text} placement="leftBottom" color="green">
+            <Button>LB</Button>
+          </Tooltip>
+        </FlexReverse>
+
+        <FlexReverse>
+          <Tooltip text={text} placement="rightTop" color="blue">
+            <Button>RT</Button>
+          </Tooltip>
+
+          <Tooltip text={text} placement="right" color="purple">
+            <Button>Right</Button>
+          </Tooltip>
+
+          <Tooltip text={text} placement="rightBottom" color="royalblue">
+            <Button>RB</Button>
+          </Tooltip>
+        </FlexReverse>
+      </Between>
+
+      <Flex>
+        <Tooltip text={text} placement="bottomLeft" color="magenta">
+          <Button>BL</Button>
+        </Tooltip>
+
+        <Tooltip text={text} placement="bottom" color="chocolate">
+          <Button>Bottom</Button>
+        </Tooltip>
+
+        <Tooltip text={text} placement="bottomRight" color="gold">
+          <Button>BR</Button>
+        </Tooltip>
+      </Flex>
     </Container>
   );
 };
@@ -27,10 +74,34 @@ const App: React.FC<Props> = ({
 export default App;
 
 const Container = styled.div`
+  width: 500px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 50vh;
+  margin-left: calc(50vw - 200px);
+  margin-top: calc(50vh - 90px);
+`;
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+
+  width: 320px;
+`;
+
+const FlexReverse = styled.div`
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const Between = styled.div`
+  width: 420px;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Button = styled.button`
@@ -39,7 +110,16 @@ const Button = styled.button`
   color: black;
 
   font-size: 14px;
+  min-width: 70px;
   height: 32px;
   padding: 4px 15px;
   border-radius: 6px;
+
+  transition: 0.5s;
+
+  &:hover {
+    color: hotpink;
+    background: cyan;
+    box-shadow: 0 0 0 4px black, 0 0 0 6px cyan;
+  }
 `;
