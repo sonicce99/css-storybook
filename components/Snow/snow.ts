19887681d8acf1c158bef74a9a75cc896dcd3b63
dongsu <sonicce99@naver.com>
2022-12-29 09:59:13 +0900

    feat: private 접근제한자 설정

--- components/Snow/snow.ts
+++ components/Snow/snow.ts
@@ -1,55 +1,55 @@
 import { css, keyframes } from "styled-components";
 
 export default class Snow {
-  _opacity: number;
-  _snowScale: number;
+  private opacity: number;
+  private snowScale: number;
 
-  _start_X_Point: number;
-  _offSet_X: number;
-  _end_X_Point: number;
+  private start_X_Point: number;
+  private offSet_X: number;
+  private end_X_Point: number;
 
-  _return_X_Point: number;
-  _return_time: number;
-  _Y_Point: number;
+  private return_X_Point: number;
+  private return_time: number;
+  private Y_Point: number;
 
-  _animation_duration: string;
-  _animation_delay: string;
+  private animation_duration: string;
+  private animation_delay: string;
 
   constructor() {
-    this._opacity = Math.random();
-    this._snowScale = Math.random();
+    this.opacity = Math.random();
+    this.snowScale = Math.random();
 
-    this._start_X_Point = Math.random() * window.innerWidth;
-    this._offSet_X = (this.getRandomValue(-5, 5) * window.innerWidth) / 100;
-    this._end_X_Point = this._start_X_Point + this._offSet_X;
-    this._return_X_Point = this._start_X_Point + this._offSet_X / 2;
+    this.start_X_Point = Math.random() * window.innerWidth;
+    this.offSet_X = (this.getRandomValue(-5, 5) * window.innerWidth) / 100;
+    this.end_X_Point = this.start_X_Point + this.offSet_X;
+    this.return_X_Point = this.start_X_Point + this.offSet_X / 2;
 
-    this._return_time = this.getRandomValue(3, 8) / 10;
-    this._Y_Point = this._return_time * 100;
+    this.return_time = this.getRandomValue(3, 8) / 10;
+    this.Y_Point = this.return_time * 100;
 
-    this._animation_duration = `${this.getRandomValue(10, 30)}s`;
-    this._animation_delay = `${Math.random() * -30}s`;
+    this.animation_duration = `${this.getRandomValue(10, 30)}s`;
+    this.animation_delay = `${Math.random() * -30}s`;
   }
 
-  getRandomValue(min: number, max: number) {
+  private getRandomValue(min: number, max: number) {
     const value = Math.random();
     const randomValue = min + Math.floor(value * (max - min + 1));
 
     return randomValue;
   }
 
-  getAnimation() {
+  private getAnimation() {
     const falling = keyframes`
-      ${`${this._Y_Point}%`} {
-          transform: translate(${this._end_X_Point}px, ${this._Y_Point}vh);
+      ${`${this.Y_Point}%`} {
+          transform: translate(${this.end_X_Point}px, ${this.Y_Point}vh);
       }
       to {
-          transform: translate(${this._return_X_Point}px, 100vh);
+          transform: translate(${this.return_X_Point}px, 100vh);
       }
       `;
 
     const animation = css`
-      animation: ${falling} ${this._animation_duration} ${this._animation_delay}
+      animation: ${falling} ${this.animation_duration} ${this.animation_delay}
         linear infinite;
     `;
 
@@ -58,8 +58,8 @@ export default class Snow {
 
   getProps() {
     return {
-      opacity: this._opacity,
-      transform: `translate(${this._start_X_Point}px, -10px) scale(${this._snowScale})`,
+      opacity: this.opacity,
+      transform: `translate(${this.start_X_Point}px, -10px) scale(${this.snowScale})`,
       animation: this.getAnimation(),
     };
   }
