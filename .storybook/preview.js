1f52443c2455e17c7afeb5f5c371ff344054db14
dongsu <sonicce99@naver.com>
2023-01-10 15:28:08 +0900

    docs : parameters에 docs 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -12,10 +12,12 @@ export const decorators = [
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
   layout: "fullscreen",
+  docs: { inlineStories: false, iframeHeight: 800 },
 };
