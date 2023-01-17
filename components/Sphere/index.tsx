16475dc00952ed40c868e375b4df8c2e8f74770f
dongsu <sonicce99@naver.com>
2023-01-17 10:20:20 +0900

    feat: Sphere 구현 완료

---
+++ components/Sphere/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+interface Props {
+  lines: number;
+  width: number;
+  height: number;
+  perspective: number;
+}
+
+type ContainerProps = Pick<Props, "width" | "height" | "perspective">;
+
+interface RingProps {
+  color: string;
+  angle: number;
+}
+
+const App: React.FC<Props> = ({ lines, width, height, perspective }) => {
+  const [array, setArray] = useState<null[]>([]);
+
+  useEffect(() => {
+    handleArray(lines);
+  }, [lines]);
+
+  const handleArray = (lines: number) => {
+    setArray(new Array(lines).fill(null));
+  };
+
+  const getRandomColor = () => {
+    return "#" + Math.round(Math.random() * 0xffffff).toString(16);
+  };
+
+  return (
+    <Container width={width} height={height} perspective={perspective}>
+      <Scene>
+        {array.map((_, index) => {
+          const color = getRandomColor();
+          return <Ring color={color} angle={index} key={index} />;
+        })}
+      </Scene>
+    </Container>
+  );
+};
+
+export default App;
+
+const sceneAnimation = keyframes`
+      to {
+    transform: rotateX(360deg) rotateY(360deg);
+  }
+`;
+
+const ringAnimation = keyframes`
+  to {
+    opacity: 1;
+  }
+`;
+
+const Container = styled.div<ContainerProps>`
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+  margin: 0 auto;
+  margin-top: calc(50vh - ${({ height }) => Math.floor(height / 2)}px);
+  perspective: ${({ perspective }) => perspective}px;
+`;
+
+const Scene = styled.div`
+  width: 100%;
+  height: 100%;
+
+  transform-style: preserve-3d;
+  position: relative;
+  transform: rotateX(0deg) rotateY(0deg);
+
+  animation: ${sceneAnimation} 7.5s 1.3s infinite linear;
+`;
+
+const Ring = styled.div<RingProps>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  border: 6px;
+  border-style: solid;
+  border-radius: 50%;
+  color: ${({ color }) => color};
+  transform: rotateY(${({ angle }) => angle * 5}deg);
+  opacity: 0;
+
+  animation: ${ringAnimation} 0.75s forwards ease-in-out;
+`;
---
+++ stories/Sphere.stories.tsx
@@ -0,0 +1,45 @@
+import App from "components/Sphere";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+export default {
+  title: "dongsu/Sphere",
+  component: App,
+
+  parameters: {
+    docs: {
+      description: { component: "3차원 구체입니다." },
+    },
+  },
+
+  argTypes: {
+    lines: {
+      description: "구체를 만드는 선의 숫자를 조절 할 수 있습니다.",
+      control: { type: "number", min: 0, max: 70, step: 10 },
+    },
+
+    width: {
+      description: "구체를 너비를 조절 할 수 있습니다.",
+      control: { type: "number", min: 0, max: 600, step: 100 },
+    },
+
+    height: {
+      description: "구체를 높이를 조절 할 수 있습니다..",
+      control: { type: "number", min: 0, max: 600, step: 100 },
+    },
+
+    perspective: {
+      description: "구체를 원근감를 조절 할 수 있습니다..",
+      control: { type: "number", min: 100, max: 600, step: 100 },
+    },
+  },
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  lines: 100,
+  width: 500,
+  height: 500,
+  perspective: 500,
+};
