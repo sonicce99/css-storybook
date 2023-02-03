d740c0c750fd45175edca5159c4e67ac7571badd
dongsu <sonicce99@naver.com>
2023-02-03 16:02:34 +0900

    docs : parameters 추가

--- stories/Barbershop.stories.tsx
+++ stories/Barbershop.stories.tsx
@@ -5,23 +5,13 @@ export default {
   title: "dongsu/Barbershop",
   component: App,
 
-  //   parameters: {
-  //     docs: {
-  //       description: { component: "3차원 원기둥입니다." },
-  //     },
-  //   },
-
-  //   argTypes: {
-  //     viewers: {
-  //       description: "시청자 수를 변경할 수 있습니다.",
-  //       control: { type: "number", min: 1, max: 30, step: 1 },
-  //     },
-  //   },
+  parameters: {
+    docs: {
+      description: { component: "이발소 입니다." },
+    },
+  },
 } as ComponentMeta<typeof App>;
 
 const Template: ComponentStory<typeof App> = (args) => <App />;
 
 export const Default = Template.bind({});
-// Default.args = {
-//   viewers: 10,
-// };
