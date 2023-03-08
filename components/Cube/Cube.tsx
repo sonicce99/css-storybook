3b454d56931b5233a2bf6f319c0b8995fe7968e3
dongsu <sonicce99@naver.com>
2023-03-08 14:55:01 +0900

    fix: div 안에 canvas 렌더링 되도록 코드 변경

--- components/Cube/Cube.tsx
+++ components/Cube/Cube.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
 import {
   Scene,
   PerspectiveCamera,
@@ -9,36 +10,43 @@ import {
 } from "three";
 
 const Cube = () => {
-  const scene = new Scene();
-  const camera = new PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-  );
-  const renderer = new WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+  const ref = useRef<HTMLDivElement>(null);
 
-  const geometry = new BoxGeometry(1, 1, 1);
-  const material = new MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new Mesh(geometry, material);
-  scene.add(cube);
+  useEffect(() => {
+    const scene = new Scene();
+    const camera = new PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  camera.position.z = 5;
+    ref.current?.appendChild(renderer.domElement);
 
-  function animate() {
-    requestAnimationFrame(animate);
+    const geometry = new BoxGeometry(1, 1, 1);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new Mesh(geometry, material);
+    scene.add(cube);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    camera.position.z = 5;
 
-    renderer.render(scene, camera);
-  }
+    function animate() {
+      requestAnimationFrame(animate);
 
-  animate();
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
 
-  return <div>Cube</div>;
+      renderer.render(scene, camera);
+    }
+
+    animate();
+  }, [ref]);
+
+  return <Canvas ref={ref} />;
 };
 
 export default Cube;
+
+const Canvas = styled.div``;
